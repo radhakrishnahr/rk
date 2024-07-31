562aad92948958c50598fe4716f95f656703e002
Radhakrishna <neravena.hr@gmail.com>
2024-07-31 10:42:54 +0530

    my second file

---
+++ rama.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Read two numbers from the user
+echo "Enter the first number: "
+read num1
+
+echo "Enter the second number: "
+read num2
+
+# Perform multiplication
+result=$((num1 * num2))
+
+# Display the result
+echo "The result of multiplying $num1 and $num2 is: $result"
